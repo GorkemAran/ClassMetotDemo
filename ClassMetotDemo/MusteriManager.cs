9072061ac297944eab0ce1ff9cafd0c831cdc762
Görkem Aran <GrkemAran@hotmail.com>
2021-01-19 22:25:42 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + "adlı müşteri sisteme başarıyla eklendi !\n");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + "adlı müşteri sistemden başarıyla silindi !\n");
+        }
+        
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("\n-----Müşteri Bilgisi-----\n");
+
+                Console.WriteLine("Müşteri adı = " + musteri.Adi);
+                Console.WriteLine("Müşteri TC'si = " + musteri.Tc);
+                Console.WriteLine("Müşteri cinsiyeti = " + musteri.Cinsiyeti);
+                Console.WriteLine("Müşteri yaşı = " + musteri.Yasi);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Ahmet";
+            musteri1.Tc = "11111111111";
+            musteri1.Cinsiyeti = "Erkek";
+            musteri1.Yasi = 28;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Ayşe";
+            musteri2.Tc = "22222222222";
+            musteri2.Cinsiyeti = "Kadın";
+            musteri2.Yasi = 35;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Ceren";
+            musteri3.Tc = "33333333333";
+            musteri3.Cinsiyeti = "Kadın";
+            musteri3.Yasi = 42;
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Adi = "Mehmet";
+            musteri4.Tc = "44444444444";
+            musteri4.Cinsiyeti = "Erkek";
+            musteri4.Yasi = 32;
+
+            Musteri[] musteriler = new Musteri[] {musteri1,musteri2,musteri3,musteri4 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Listele(musteriler);
+
+
+        }
+    }
+}
